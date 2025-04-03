2c2987cfb9d6bd56768392fc67e6f2fb426ad563
NKD52 <coolnxx5152@gmail.com>
2025-04-04 00:34:32 +0500

    change letterchip font size and style, also added ref feature and confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import { clsx } from "clsx";
 import Confetti from "react-confetti";
 import Guess from "./Guess";
@@ -14,7 +14,13 @@ export default function AssemblyEndgame() {
     const [lost, setLost] = useState(puzzle.lost)
     const [won, setWon] = useState(puzzle.won)
 
+    const myRef = useRef(null)
     
+    useEffect(()=>{
+        if(myRef.current){
+            myRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    },[won])
    
     function addGuessLetter(letter){
         if (puzzle.guesses[puzzle.currentGuess].length < 7 && /^[A-Za-z]$/.test(letter)) {
@@ -46,15 +52,20 @@ export default function AssemblyEndgame() {
         return () => window.removeEventListener("keyup", handleKeyupBound);
     }, []);
 
-    console.log(puzzle.word.toLowerCase())
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
     const keyboardElements = alphabet.split("").map((letter) => (
         <button className="new-game" onClick = {()=>{addGuessLetter(letter)}} key={letter}>{letter.toUpperCase()}</button>
     ));
-    console.log(puzzle.lost)
     return (
         <main>
+            {
+                puzzle.won && 
+                    <Confetti
+                        recycle={false}
+                        numberOfPieces={1000}
+                    />
+            }
             <header>
                 <h1>Emojidle</h1>
                 <p>Guess the word within 5 attempts!</p>
@@ -69,11 +80,11 @@ export default function AssemblyEndgame() {
             </section>
 
             <footer>
-                <h1>{puzzle.lost ? "You lost, lol" : ""}</h1>
+                <h1>{puzzle.lost ? `You lost, the word was ${puzzle.word}` : ""}</h1>
                 <h1>{puzzle.won ? "You Won, Champion!" : ""}</h1>
 
             </footer>
-            <section className="keyboard">{keyboardElements}</section>
+            <section ref = {myRef} className="keyboard">{keyboardElements} </section>
             {(lost||won) && <button className="play-again" onClick={() => { puzzle.init(); setGuesses([...puzzle.guesses]); setCurrentGuess(puzzle.currentGuess), setWon(puzzle.won),setLost(puzzle.lost) }}>Play Again</button>}
 
             {/* <p>Word: {puzzle.word}</p>
